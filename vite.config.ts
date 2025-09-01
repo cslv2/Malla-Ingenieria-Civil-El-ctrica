32bf03190030c6ad58814369a8d76739b7fec608
Camila Silva <camilaa.silvaa.r@gmail.com>
2025-09-01 16:21:57 -0400

    build: preparar carpeta docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Malla-Ingenieria-Civil-El-ctrica/', // <- igualito al nombre del repo
+  base: '/Malla-Ingenieria-Civil-El-ctrica/',
 });
